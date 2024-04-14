d441638317040b7d306359e251bd848a141cad31
Manthri-Anusha <137473693+Manthri-Anusha@users.noreply.github.com>
2024-04-14 22:38:59 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../components/Footer.css";
+
+function Footer() {
+  return (
+      <footer className="Footer">
+        <p>© 2024 sharing platform. All rights reserved.</p>
+      </footer>
+  );
+}
+
+export default Footer;
