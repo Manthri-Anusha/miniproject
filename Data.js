45bcd8459349cc69e04f9cdc02d39efdf5dbfc55
Manthri-Anusha <137473693+Manthri-Anusha@users.noreply.github.com>
2024-04-14 22:29:22 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,23 @@
+export const krithi=[
+    {
+        image: "/images/kr6.jpg",
+        title: "kr1",
+    },
+    // {
+    //     image: "/images/kr7.jpeg",
+    //     title: "kr2",
+    // },
+    {
+        image: "/images/kr3.jpg",
+        title: "kr3",
+    },
+    {
+        image: "/images/kr4.jpg",
+        title: "kr4",
+    },
+    {
+        image: "/images/kr5.jpg",
+        title: "kr5",
+    },
+
+] 
