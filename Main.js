8e389d523cf4b9d9f2ef288534ab6ca55128bbb5
Manthri-Anusha <137473693+Manthri-Anusha@users.noreply.github.com>
2024-04-15 23:27:45 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
-import Carousel2 from './Carousel2';
-import About from '../About';
-import Footer from './Footer';
-import { krithi } from './Data';
-import Contact from './Contact';
-import "./Main.css";
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Carousel from "./components/Carousel2";
+import SignIn from "./Sign";
+import AboutUs from './About'; // Adjust import paths as necessary
+import { krithi } from "./components/Data";
 
-function Main()
-{
-    return(
-        <div>
-            <div className="Site-content">
-            <Carousel2 images={krithi} />
-            </div>
-            <About />
-            <div className="phone" id="contact">
-                <h1>Contact Us</h1>
-                <Contact /></div>
-            <Footer />
+function MainContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      <Routes>
+        <Route path="./Sign.js" element={<SignIn />} />
+        <Route path="./About.js" element={<AboutUs />} />
+      </Routes>
+      {/* Conditionally render content based on the route */}
+      {location.pathname !== './Sign.js' && (
+        <div className="Site-content">
+          <Carousel images={krithi} />
+          <AboutUs />
+          {/* Any other content to conditionally display */}
         </div>
-    );
+      )}
+    </>
+  );
 }
 
-export default Main;
+export default MainContent;
